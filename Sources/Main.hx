bf78af82bbe5949f617a39770ed670bd99a92290
lucasl <lucasl@oktana.io>
2020-07-14 11:39:34 -0300

    add new khawy version

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -1,6 +1,7 @@
 package;
 
 import kha.WindowMode;
+import com.target.Html5;
 import com.framework.Simulation;
 import kha.System;
 import kha.System.SystemOptions;
@@ -13,6 +14,7 @@ class Main {
 		#if hotml
 		new hotml.Client();
 		#end
+		Html5.fillScreen();
 		var windowsOptions = new WindowOptions("Defend Antathaan", 0, 0, 500, 720, null, true, WindowFeatures.FeatureResizable, WindowMode.Windowed);
 		var frameBufferOptions = new FramebufferOptions();
 		System.start(new SystemOptions("Defend Antathaan", 500, 720, windowsOptions, frameBufferOptions), function(w) {
